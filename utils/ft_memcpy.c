8397fe9109a879fd9f21c7c0c61199db6ad38778
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-11-14 01:19:16 -1200

    Create ft_memcpy.c

---
+++ utils/ft_memcpy.c
@@ -0,0 +1,21 @@
+#include "../Headers/TRODO.h"
+#include <stdlib.h>
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*source;
+	unsigned char	*dest;
+
+	if (!dst || !src)
+		return (NULL);  // Null-check for SDL compatibility
+	i = 0;
+	source = (unsigned char *)src;
+	dest = (unsigned char *)dst;
+	while (i < n)
+	{
+		dest[i] = source[i];
+		i++;
+	}
+	return (dst);
+}

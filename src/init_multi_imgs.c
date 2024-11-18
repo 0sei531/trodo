ca757e12277d050241ed637607f9e89fdf0f7f3e
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-11-18 10:16:28 -1200

    Update init_multi_imgs.c

--- src/init_multi_imgs.c
+++ src/init_multi_imgs.c
@@ -1,6 +1,12 @@
 #include "../Headers/TRODO.h"
+#include "../Headers/structs.h"
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define MAX_FRAMES 100
 
 
 void path_name2(char *path, int i, int len)
@@ -15,10 +21,26 @@ void path_name2(char *path, int i, int len)
     path[j++] = 0;
 }
 
+static void allocate_gun_frames(int o, int num_frames)
+{
+    if (!data.gun[o].gun) {
+        data.gun[o].gun = calloc(num_frames, sizeof(t_img));
+        if (!data.gun[o].gun) {
+            SDL_Log("Failed to allocate memory for gun frames");
+            return;
+        }
+    }
+}
+
 void init_gun(int o, int len, char *path)
 {
     int i;
     SDL_Surface *surface;
+
+    allocate_gun_frames(o, len);
+    if (!data.gun[o].gun) {
+        return;
+    }
 
     i = 0;
     while (++i < len)
@@ -30,16 +52,9 @@ void init_gun(int o, int len, char *path)
             SDL_Log("Error loading image %s: %s", path, IMG_GetError());
             return;
         }
-
-        // Make sure gun[o].gun is properly allocated before using
-        if (!data.gun[o].gun) {
-            SDL_Log("Error: gun array not allocated for index %d", o);
-            SDL_FreeSurface(surface);
-            return;
-        }
-
-        data.gun[o].gun[i - 1].tex = SDL_CreateTextureFromSurface(data.sdl.renderer, surface);
-        if (!data.gun[o].gun[i - 1].tex) {
+        
+        data.gun[o].gun[i - 1].texture = SDL_CreateTextureFromSurface(data.sdl.renderer, surface);
+        if (!data.gun[o].gun[i - 1].texture) {
             SDL_Log("Error creating texture: %s", SDL_GetError());
         }
         SDL_FreeSurface(surface);
@@ -75,12 +90,12 @@ static void init_single_motion(int o, int len, char *path, int path_len)
     int i;
     SDL_Surface *surface;
 
-    i = 0;
     if (!data.motion || !data.motion[o].frm) {
         SDL_Log("Error: motion array not properly allocated");
         return;
     }
 
+    i = 0;
     while (++i < len)
     {
         path_name2(path, i, path_len);
@@ -90,9 +105,9 @@ static void init_single_motion(int o, int len, char *path, int path_len)
             SDL_Log("Error loading image %s: %s", path, IMG_GetError());
             return;
         }
-
-        data.motion[o].frm[i - 1].tex = SDL_CreateTextureFromSurface(data.sdl.renderer, surface);
-        if (!data.motion[o].frm[i - 1].tex) {
+        
+        data.motion[o].frm[i - 1].texture = SDL_CreateTextureFromSurface(data.sdl.renderer, surface);
+        if (!data.motion[o].frm[i - 1].texture) {
             SDL_Log("Error creating texture: %s", SDL_GetError());
         }
         SDL_FreeSurface(surface);
@@ -100,8 +115,31 @@ static void init_single_motion(int o, int len, char *path, int path_len)
     data.motion[o].frame = len - 1;
 }
 
+static void allocate_motion_arrays(void)
+{
+    data.motion = calloc(7, sizeof(t_motion));
+    if (!data.motion) {
+        SDL_Log("Failed to allocate motion array");
+        return;
+    }
+
+    for (int i = 0; i < 7; i++) {
+        data.motion[i].frm = calloc(MAX_FRAMES, sizeof(t_img));
+        if (!data.motion[i].frm) {
+            SDL_Log("Failed to allocate motion frames for index %d", i);
+            cleanup_motion();  // Clean up previously allocated memory
+            return;
+        }
+    }
+}
+
 void init_motion(void)
 {
+    allocate_motion_arrays();
+    if (!data.motion) {
+        return;
+    }
+
     init_single_motion(0, 17, "img/enemies/run/", 16);
     init_single_motion(1, 29, "img/enemies/walk/", 17);
     init_single_motion(2, 51, "img/enemies/attack_1/", 21);
@@ -109,4 +147,22 @@ void init_motion(void)
     init_single_motion(4, 87, "img/enemies/attack_3/", 21);
     init_single_motion(5, 34, "img/enemies/hit/", 16);
     init_single_motion(6, 82, "img/enemies/death/", 18);
+}
+
+void cleanup_motion(void)
+{
+    if (data.motion) {
+        for (int i = 0; i < 7; i++) {
+            if (data.motion[i].frm) {
+                for (int j = 0; j < MAX_FRAMES; j++) {
+                    if (data.motion[i].frm[j].texture) {
+                        SDL_DestroyTexture(data.motion[i].frm[j].texture);
+                    }
+                }
+                free(data.motion[i].frm);
+            }
+        }
+        free(data.motion);
+        data.motion = NULL;
+    }
 }

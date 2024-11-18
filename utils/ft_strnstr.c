c25ca187f834663494e0c3530b6fa9a47eff1682
0sei531 <141256948+0sei531@users.noreply.github.com>
2024-11-18 10:05:46 -1200

    Update ft_strnstr.c

--- utils/ft_strnstr.c
+++ utils/ft_strnstr.c
@@ -1,30 +1,27 @@
 #include "../Headers/TRODO.h"
 #include <SDL2/SDL.h>
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+
+size_t ft_strlen(const char *str)
 {
-	size_t	i;
-	size_t	j;
+    size_t i;
 
-	// Check for NULL pointers and log errors using SDL_Log
-	if (!haystack || !needle)
-	{
-		SDL_Log("Error: NULL pointer passed to ft_strnstr");
-		return (NULL);  // Return NULL if either haystack or needle is NULL
-	}
+    if (!str)
+        return (0);
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
 
-	i = 0;
-	if (!*needle)  // If the needle is empty, return the haystack
-		return ((char *)haystack);
+size_t ft_strlen2(char **str)
+{
+    size_t i;
 
-	while (haystack[i] && i < len)
-	{
-		j = 0;
-		while (needle[j] && j + i < len && haystack[i + j] == needle[j])
-			j++;
-		if (!needle[j])  // If all characters of needle are found, return the substring
-			return (&((char *)haystack)[i]);
-		i++;
-	}
-	return (NULL);  // Return NULL if needle is not found in the haystack within len
+    if (!str)
+        return (0);
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
 }
